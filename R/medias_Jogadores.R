926dcf202d32a9feec1a9fda503e35f5955cca24
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-02-09 17:31:48 -0300

    update

--- R/medias_Jogadores.R
+++ R/medias_Jogadores.R
@@ -1,20 +1,30 @@
 #' Médias dos jogadores
 #' Função que calcula as médias individuais para os dez jogadores de uma determinada partida.
 #' Para utilizar: medias_Jogadores()
+#' @export
 # Função medias_jogadores ---------------------------------------------------------------------------------
-medias_Jogadores <- function (url_jogador) {
+medias_Jogadores <- function(url_jogador){
+
   html_lido <- read_html(as.character(url_jogador))
-  
+
   dados_jogador <- html_nodes(html_lido, 'table') %>%
     html_table()
   dados_jogador <- dados_jogador %>% map_df(as_tibble, .name_repair = 'minimal') %>%
-    dplyr::select(Rating, ACS, KAST, 'K:D', ADR)
-  
+    dplyr::select(Use, Rating, ACS, KAST, 'K:D', ADR)
+
+  dados_jogador$Use <- as.numeric(gsub(".*\\((.*)\\).*", "\\1", dados_jogador$Use))
+
   dados_jogador$KAST <- parse_number(dados_jogador$KAST)
-  
-  means_jogador <- round(colMeans(dados_jogador, na.rm = T), 2)
-  
-  means_jogador[['KAST']] <- round(means_jogador[['KAST']], 0)
-  
-  return(means_jogador)
+
+  dados_jogador[,2:ncol(dados_jogador)] <- lapply(dados_jogador[,2:ncol(dados_jogador)],
+                                                  function(x, y) x * y, y = dados_jogador$Use)
+
+  dados_jogador <- lapply(dados_jogador, sum, na.rm = T)
+
+  dados_jogador <- lapply(dados_jogador, function(x, y) round(x / y, 2), dados_jogador$Use)
+
+  medias_jogador <- dados_jogador
+
+  medias_jogador[['KAST']] <- round(medias_jogador[['KAST']], 0)
+  return(medias_jogador)
 }
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 #### Pacote de funções da linguagem R criado para utilização no projeto de Redes neurais para o cenário competitivo de Valorant. 
 #### Contém funções que facilitam a obtenção de dados e o manuseio dos mesmos.
+
+
+##Pacote utilizados:
+library(devtools)
+library(roxygen2)
