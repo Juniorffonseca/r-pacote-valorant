09169d4523da279a6c8ecec20c276ac41130dcce
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-02-10 20:08:39 -0300

    nova funcao

---
+++ R/get_Ganhadores.R
@@ -0,0 +1,19 @@
+#' get_Ganhadores
+#' Função que identifica o ganhador de determinada partida.
+#' Para utilizar: get_Ganhadores(url)
+#' @export
+# Função get_Ganhadores ---------------------------------------------------------------------------------
+get_Ganhadores <- function(url_partida){
+  placar <- read_html(url_partida) %>% 
+    html_nodes("div.js-spoiler") %>% html_text(trim=T)
+  
+  placar <- str_replace_all(placar, '\t', '') %>% str_replace_all('\n', '')
+  
+  placar <- as.data.frame(placar[1])
+  
+  placar <- separate(placar, 'placar[1]', into = c('Time1', 'Time2'), sep = ':', extra = 'merge')
+  
+  ifelse(placar$Time1 > placar$Time2, ganhador <- 1, ganhador <- 0)
+  
+  return(ganhador)
+}
