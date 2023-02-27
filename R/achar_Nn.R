b37bc233735e6f4e30650c222075f3fede284845
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-02-27 13:48:42 -0300

    nova funcao

---
+++ R/achar_Nn.R
@@ -0,0 +1,29 @@
+#' achar_Nn
+#' Função que itera para achar uma rede neural.
+#' Para utilizar: achar_Nn()
+#' @export
+achar_Nn <- function(){
+
+  n <<- neuralnet(ganhador == 1 ~ .,
+                  data = training_data,
+                  hidden = c(10),
+                  err.fct = "sse",
+                  linear.output = F,
+                  threshold = 1,
+                  lifesign = 'minimal',
+                  rep = 1,
+                  algorithm = 'rprop-',
+                  stepmax = 10000)
+
+  Predict <<- compute(n, test_data)
+
+  nn2 <<- ifelse(Predict$net.result[,1]>0.5,1,0)
+
+  predictVstest <<- cbind(test_data, Predict$net.result)
+  i <<- sum(predictVstest$ganhador == nn2)/ nrow(test_data)
+  print(i)
+
+  z <<- ifelse(i>z, z <<- i, z <<- z)
+
+  print(z)
+}
