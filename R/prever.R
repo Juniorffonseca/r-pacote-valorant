2555e80f851d230c63d7dab9713c6d50033827a6
Junior Fonseca <edinaldoffjunior@gmail.com>
2023-03-13 18:45:39 -0300

    .

--- R/prever.R
+++ R/prever.R
@@ -23,35 +23,6 @@ prever <- function(link){
 
   previsao <- previsao$net.result[1]
 
-  partida_reversa <- partida
-
-  partida_reversa$time1R <- partida$time2R
-  partida_reversa$time2R <- partida$time1R
-  partida_reversa$time1ACS <- partida$time2ACS
-  partida_reversa$time2ACS <- partida$time1ACS
-  partida_reversa$time1KAST <- partida$time2KAST
-  partida_reversa$time2KAST <- partida$time1KAST
-  partida_reversa$time1KD <- partida$time2KD
-  partida_reversa$time2KD <- partida$time1KD
-  partida_reversa$time1ADR <- partida$time2ADR
-  partida_reversa$time2ADR <- partida$time1ADR
-
-  previsao2 <- compute(n, partida_reversa)
-
-  previsao2 <- previsao2$net.result[1]
-
-  a <- previsao
-  b <- previsao2
-
-  transforma_probabilidade <- function (y, x){
-    z = y / (y + x)
-    w = x / (x + y)
-    c = as.matrix(c(z,w))
-    return(c)
-  }
-
-  previsao <- transforma_probabilidade(a,b)
-
   previsao <- previsao * 100
 
   return(previsao)
